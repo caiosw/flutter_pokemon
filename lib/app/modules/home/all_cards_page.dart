8d77007b5bde82c2581a5957a5de839787a3c99c
Caio Werneck <caiosw@gmail.com>
2020-10-04 01:28:57 -0300

    Navegação entre páginas

--- lib/app/modules/home/all_cards_page.dart
+++ lib/app/modules/home/all_cards_page.dart
@@ -1,17 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
-import 'package:pokemon_dio/app/modules/home/all_cards_page.dart';
 import 'detail_page.dart';
 import 'domain/pokemon.dart';
 import 'home_controller.dart';
 
-class HomePage extends StatefulWidget {
+class AllCardsPage extends StatefulWidget {
 
   @override
-  _HomePageState createState() => _HomePageState();
+  _AllCardsPageState createState() => _AllCardsPageState();
 }
 
-class _HomePageState extends ModularState<HomePage, HomeController> {
+class _AllCardsPageState extends ModularState<AllCardsPage, HomeController> {
   //use 'controller' variable to access controller
 
   List<Pokemon> pokemons = [
@@ -38,36 +37,27 @@ class _HomePageState extends ModularState<HomePage, HomeController> {
     ),
 
   ];
-
-  int _currentIndex = 0;
-
-  List<Widget> pages = [
-    AllCardsPage(),
-    Container(),
-    Container()
-  ];
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _currentIndex,
-        onTap: (index) => changePage(index),
-        items: [
-          BottomNavigationBarItem(
-              icon: Icon(Icons.all_inclusive),
-              label: "Cards"
-          ),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.favorite),
-              label: "Favoritos"
-          ),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.person),
-              label: "Obtidos"
-          )
-        ],),
-      body: pages[_currentIndex],
+      body: GridView.count(
+        childAspectRatio: 0.72,
+        crossAxisCount: 2,
+        children: List.generate(pokemons.length, (index) {
+          var pokemon = pokemons[index];
+
+          return GestureDetector(
+            onTap: () => openPageDetail(pokemon),
+            child: Padding(
+              padding: const EdgeInsets.only(bottom: 5.0),
+              child: Hero(
+                tag: pokemon.id,
+                child: Image.network(pokemon.imageUrl)
+              )
+            )
+          );
+        }),
+      ),
     );
   }
 
@@ -77,12 +67,6 @@ class _HomePageState extends ModularState<HomePage, HomeController> {
           return PageDetail(pokemon: pokemon);
         })
     );
-  }
-
-  changePage(int index) {
-    setState(() {
-      _currentIndex = index;
-    });
   }
 }
 
